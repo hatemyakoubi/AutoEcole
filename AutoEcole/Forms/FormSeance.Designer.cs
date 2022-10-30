70a48aacd06bcad472f174f515b1c206bb40d200
Fayna <Fayna@DESKTOP-0S32ARS>
2022-10-30 20:33:03 +0100

    ajouter button de test

--- AutoEcole/Forms/FormSeance.Designer.cs
+++ AutoEcole/Forms/FormSeance.Designer.cs
@@ -30,6 +30,7 @@ namespace AutoEcole.Forms
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,21 @@ namespace AutoEcole.Forms
             this.label1.TabIndex = 0;
             this.label1.Text = "Seances";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormSeance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "FormSeance";
             this.Text = "Séances";
@@ -58,5 +69,6 @@ namespace AutoEcole.Forms
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
